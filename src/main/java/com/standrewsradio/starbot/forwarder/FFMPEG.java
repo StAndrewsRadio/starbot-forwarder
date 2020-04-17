77f86633bebafab14804333114e6b5061e57eea9
Kezz101 <kieran.wallbanks@gmail.com>
2020-04-17 13:17:09 +0100

    Correct ffmpeg input formatting

--- src/main/java/com/standrewsradio/starbot/forwarder/FFMPEG.java
+++ src/main/java/com/standrewsradio/starbot/forwarder/FFMPEG.java
@@ -9,9 +9,14 @@ import java.lang.ProcessBuilder.Redirect;
 public class FFMPEG {
     private final Process process;
 
+    /**
+     * Creates a new process-backed instance of ffmpeg.
+     * @param arguments the command line arguments
+     * @throws IOException if an error occurred whilst starting the process
+     */
     public FFMPEG(Arguments arguments) throws IOException {
         this.process = new ProcessBuilder()
-                .command("ffmpeg", "-re", "-ac", arguments.singleChannel ? "1" : "2", "-f", "s32le", "-i", "pipe:0",
+                .command("ffmpeg", "-hide_banner", "-re", "-f", "s16be", "-ac", "2", "-ar", "48000", "-i", "pipe:0",
                         "-ar", String.valueOf(arguments.sampleRate), "-ac", String.valueOf(arguments.compressionLevel),
                         "-c:a", "libmp3lame", "-f", "mp3", "-reconnect_at_eof", "1", "-reconnect_streamed", "1",
                         "-reconnect", "1", "-reconnect_delay_max", "1000", "-content_type", "audio/mpeg",
